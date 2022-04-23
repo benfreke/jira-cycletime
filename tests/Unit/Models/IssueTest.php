797a9dc88fad2e1dbb8ebdf87ad6ad24b68b9e58
Ben <benfreke@users.noreply.github.com>
2022-04-23 15:47:47 +1000

    test(issue): adds test for getLastUpdatedDate

--- tests/Unit/Models/IssueTest.php
+++ tests/Unit/Models/IssueTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Models;
 
 use App\Models\Issue;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -15,6 +16,17 @@ class IssueTest extends TestCase
      */
     public function testGetLastUpdatedDate()
     {
+        // Test null when no date is set
         self::assertNull(Issue::getLastUpdatedDate());
+
+        // Check for a known time in the past
+        Issue::factory()->create([
+            'last_jira_update' => Carbon::now()->subHours(5)->subMinute()
+        ]);
+        self::assertEquals(5, Issue::getLastUpdatedDate());
+        Issue::factory()->create([
+            'last_jira_update' => Carbon::now()->subHours(5)->addMinute()
+        ]);
+        self::assertEquals(4, Issue::getLastUpdatedDate());
     }
 }
