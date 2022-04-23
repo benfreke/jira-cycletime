ff4d8c9e5e779ca16fcbb4eeaea0409b3c0c3a22
Ben <benfreke@users.noreply.github.com>
2022-04-23 14:21:35 +1000

    test: adds tests
    Restructured some code to support the tests!

--- app/Console/Commands/CycleTimeIssues.php
+++ app/Console/Commands/CycleTimeIssues.php
@@ -3,9 +3,9 @@
 namespace App\Console\Commands;
 
 use App\Models\Issue;
+use App\Services\Jira;
 use Exception;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Http;
 
 class CycleTimeIssues extends Command
 {
@@ -28,7 +28,7 @@ class CycleTimeIssues extends Command
      *
      * @return void
      */
-    public function __construct(private int $resultsToGet = 60)
+    public function __construct(private readonly int $resultsToGet = 60)
     {
         parent::__construct();
     }
@@ -40,23 +40,15 @@ class CycleTimeIssues extends Command
      */
     public function handle(): int
     {
-        $response = Http::withBasicAuth(config('cycletime.jira-user'), config('cycletime.token'))
-            ->acceptJson()->get(config('cycletime.jira-url') . 'rest/api/3/search', [
-                'jql' => $this->getJql(),
-                'startAt' => 0,
-                'maxResults' => $this->resultsToGet,
-                'fields' => [
-                    'summary',
-                    'statusCategory',
-                ],
-            ]);
+        $jiraService = new Jira($this->resultsToGet);
 
-        $totalFound = count($response->json('issues'));
+        $results = $jiraService->getIssues();
+        $totalFound = count($results);
 
         $this->info("Attempting to find $this->resultsToGet issues");
         $this->info("Total found $totalFound");
 
-        foreach ($response->json('issues') as $issue) {
+        foreach ($results as $issue) {
             try {
                 // Make sure all potential values exist
                 if (empty($issue['fields']['assignee'])) {
@@ -89,40 +81,4 @@ class CycleTimeIssues extends Command
         return Command::SUCCESS;
     }
 
-    /**
-     * Get all the issues in our projects, that are completed
-     *
-     * @return string
-     */
-    private function getJql(): string
-    {
-        $jql = 'project IN ("AGENCY","PLAN","UNS","CBW")';
-        $jql .= ' AND statuscategory = "Complete"';
-        $updatedHours = $this->getLastUpdatedDate();
-        if ($updatedHours) {
-            $jql .= " AND updated >= $updatedHours";
-        }
-
-        $jql .= ' ORDER BY updated ASC';
-
-        return $jql;
-    }
-
-    /**
-     * @return string|null The last updated time we have in the DB, in hours
-     */
-    private function getLastUpdatedDate(): ?string
-    {
-        $lastUpdatedIssue = Issue::latest('last_jira_update')->first();
-        if (!isset($lastUpdatedIssue->last_jira_update)) {
-            return null;
-        }
-        $hours = $lastUpdatedIssue->last_jira_update->diffInHours();
-        if ($hours) {
-            // Add an extra hour to ensure we don't miss anything
-            $hours++;
-            return "-${hours}h";
-        }
-        return null;
-    }
 }
---
+++ app/Services/Jira.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Issue;
+use Illuminate\Support\Facades\Http;
+
+class Jira
+{
+    public function __construct(private readonly int $resultsToGet = 60)
+    {
+
+    }
+
+    /**
+     * Get the issues from Jira
+     *
+     * @return array
+     */
+    public function getIssues(): array
+    {
+        $response = Http::withBasicAuth(config('cycletime.jira-user'), config('cycletime.token'))
+            ->acceptJson()->get(config('cycletime.jira-url') . config('cycletime.jira-url'), [
+                'jql' => $this->getJql(),
+                'startAt' => 0,
+                'maxResults' => $this->resultsToGet,
+                'fields' => [
+                    'summary',
+                    'statusCategory',
+                ],
+            ]);
+
+        return $response->json('issues');
+    }
+
+    /**
+     * Get all the issues in our projects, that are completed
+     *
+     * @return string
+     */
+    public function getJql(): string
+    {
+        $jql = 'project IN ' . config('cycletime.jira-categories');
+        $jql .= ' AND statuscategory = "Complete"';
+        $updatedHours = $this->getLastUpdatedDate();
+        if ($updatedHours) {
+            $jql .= " AND updated >= $updatedHours";
+        }
+
+        $jql .= ' ORDER BY updated ASC';
+
+        return $jql;
+    }
+
+
+    /**
+     * @return string|null The last updated time we have in the DB, in hours
+     */
+    private function getLastUpdatedDate(): ?string
+    {
+        $hours = Issue::getLastUpdatedDate();
+        if (is_numeric($hours)) {
+            $hours++;
+            return "-${hours}h";
+        }
+        return $hours;
+    }
+}
