4126b5a23536b7773c65e99bfe08cf27cdfb4da9
Ben <benfreke@users.noreply.github.com>
2022-04-23 15:47:47 +1000

    test(issue): adds test for getLastUpdatedDate

--- database/factories/IssueFactory.php
+++ database/factories/IssueFactory.php
@@ -2,10 +2,16 @@
 
 namespace Database\Factories;
 
+use App\Models\Issue;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class IssueFactory extends Factory
 {
+    /**
+     * @var string
+     */
+    protected $model = Issue::class;
+
     /**
      * Define the model's default state.
      *
