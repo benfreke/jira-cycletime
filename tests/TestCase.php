e8aef61c2fe195809887183e5d07f8fa234aa460
Ben <benfreke@users.noreply.github.com>
2022-04-29 23:12:49 +1000

    test: adds contract test for jira

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,29 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Gets a specific fixture for testing purposes
+     * @param $fileName
+     *
+     * @return mixed
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function getFixture($fileName): mixed
+    {
+        return json_decode(\Storage::drive('fixtures')->get($fileName), true);
+    }
+
+    /**
+     * Save a fixture for testing
+     *
+     * @param  string  $fileName
+     * @param  string  $contents
+     *
+     * @return bool
+     */
+    public function setFixture(string $fileName, string $contents): bool
+    {
+        return \Storage::drive('fixtures')->put($fileName, $contents);
+    }
 }
