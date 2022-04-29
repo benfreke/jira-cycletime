ec42c4af2b67f2d1729404cee80201be2fe1cd9f
Ben <benfreke@users.noreply.github.com>
2022-04-29 23:08:02 +1000

    feat: adds fixtures driver for tests

--- config/filesystems.php
+++ config/filesystems.php
@@ -35,6 +35,11 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'fixtures' => [
+            'driver' => 'local',
+            'root' => base_path('tests' . DIRECTORY_SEPARATOR . 'Fixtures')
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
