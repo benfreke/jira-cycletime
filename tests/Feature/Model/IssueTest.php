d7dd60235b4e6a4fa9492b87e538310176312c0b
Ben <benfreke@users.noreply.github.com>
2022-05-24 21:46:31 +1000

    test: move cycletime calculation to job

--- tests/Feature/Model/IssueTest.php
+++ tests/Feature/Model/IssueTest.php
@@ -56,30 +56,25 @@ class IssueTest extends TestCase
     {
         // Arrange
         Carbon::setTestNow(Carbon::create(2022, 6, 15, 12, 30));
-        Issue::factory()->has(Transition::factory()->pastDates())->create(
-            ['issue_id' => 'past', 'last_jira_update' => Carbon::now()->subMinutes(1)]
+        Issue::factory()->has(Transition::factory()->setValidStartDone()->setUpdatedAtPast())->create(
+            ['issue_id' => 'past', 'last_jira_update' => Carbon::now()->subMinute()]
         );
-        Issue::factory()->has(Transition::factory()->pastDates())->create(
+        Issue::factory()->has(Transition::factory()->setValidStartDone()->setUpdatedAtPast())->create(
             ['issue_id' => 'past2', 'last_jira_update' => Carbon::now()->subMinutes(2)]
         );
-        Issue::factory()->has(Transition::factory()->pastDates())->create(
+        Issue::factory()->has(Transition::factory()->setValidStartDone()->setUpdatedAtFuture())->create(
             ['issue_id' => 'future', 'last_jira_update' => Carbon::now()->addMinute()]
         );
 
-        // Act
-        $results = Issue::needsNewCycleTime();
-
         // Assert
-        self::assertEquals(1, $results->count());
+        self::assertEquals(1, Issue::needsNewCycleTime()->count());
         unset($results);
 
         // Now change a transition to be later than the issue updated at
-
-        Issue::factory()->has(Transition::factory()->pastDates())->create(
+        Issue::factory()->has(Transition::factory()->setValidStartDone()->setUpdatedAtFuture())->create(
             ['issue_id' => 'future2', 'last_jira_update' => Carbon::now()->addMinute()]
         );
 
-        $results = Issue::needsNewCycletime();
-        self::assertEquals(2, $results->count());
+        self::assertEquals(2, Issue::needsNewCycletime()->count());
     }
 }
