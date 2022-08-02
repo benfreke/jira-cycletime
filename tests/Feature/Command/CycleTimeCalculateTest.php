21ba1c6bc874aae6e646a8095fb64084a4b305cd
Ben <benfreke@users.noreply.github.com>
2022-08-02 12:10:03 +1000

    test: update layout ahead of writing real tests

--- tests/Feature/Command/CycleTimeCalculateTest.php
+++ tests/Feature/Command/CycleTimeCalculateTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Command;
 
 use App\Models\Issue;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Queue;
 use Tests\TestCase;
 
 class CycleTimeCalculateTest extends TestCase
@@ -15,15 +16,22 @@ class CycleTimeCalculateTest extends TestCase
      *
      * @return void
      */
-    public function testCycleTimeCalculate()
+    public function testCycleTimeCalculate(): void
     {
-        // Arrange
+        Queue::fake();
 
-        // Act
+        // Call the method with nothing in the database
         $this->artisan('cycletime:calculate')->assertSuccessful();
-
-        // Assert
         $results = Issue::needsNewCycletime();
+
+        // Assert no results and nothing on the queue
         self::assertEquals(0, $results->get()->count());
+        Queue::assertNothingPushed();
+
+        // @todo 1 to add to the queue (done is not null)
+
+        // @todo 1 to not add to the queue (done is null)
     }
+
+
 }
