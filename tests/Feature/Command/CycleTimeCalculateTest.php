ddcdc37d6f19e14c62f961985f50f7306f40bcaf
Ben <benfreke@users.noreply.github.com>
2022-08-02 14:38:21 +1000

    test: adds test for needs cycletime update

--- tests/Feature/Command/CycleTimeCalculateTest.php
+++ tests/Feature/Command/CycleTimeCalculateTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Feature\Command;
 
+use App\Jobs\UpdateCycleTime;
 use App\Models\Issue;
+use App\Models\Transition;
+use Carbon\CarbonImmutable;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Queue;
 use Tests\TestCase;
@@ -29,8 +32,19 @@ class CycleTimeCalculateTest extends TestCase
         Queue::assertNothingPushed();
 
         // @todo 1 to add to the queue (done is not null)
+        $start = CarbonImmutable::parse('2022-05-23 08:00:00');
+        $done = CarbonImmutable::parse('2022-05-27 09:00:00');
+        /** @var $issue */
+        Issue::factory()->has(Transition::factory(['start' => $start, 'done' => $done]))->create(
+            ['issue_id' => 'fake', 'last_jira_update' => CarbonImmutable::now()]
+        );
+        $results = Issue::needsNewCycletime();
+        self::assertEquals(1, $results->get()->count());
+        $this->artisan('cycletime:calculate')->assertSuccessful();
+        Queue::assertPushed(UpdateCycleTime::class, 1);
 
         // @todo 1 to not add to the queue (done is null)
+
     }
 
 
