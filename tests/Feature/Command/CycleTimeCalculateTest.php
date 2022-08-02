48bdede8efbbeac8a702ea685f9e0628cfbc5adf
Ben <benfreke@users.noreply.github.com>
2022-08-02 15:50:24 +1000

    test: adds more test cases

--- tests/Feature/Command/CycleTimeCalculateTest.php
+++ tests/Feature/Command/CycleTimeCalculateTest.php
@@ -7,7 +7,7 @@ use App\Models\Issue;
 use App\Models\Transition;
 use Carbon\CarbonImmutable;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Queue;
+use Illuminate\Support\Facades\Queue;
 use Tests\TestCase;
 
 class CycleTimeCalculateTest extends TestCase
@@ -19,7 +19,7 @@ class CycleTimeCalculateTest extends TestCase
      *
      * @return void
      */
-    public function testCycleTimeCalculate(): void
+    public function testCycleTimeCalculateNothing(): void
     {
         Queue::fake();
 
@@ -30,8 +30,13 @@ class CycleTimeCalculateTest extends TestCase
         // Assert no results and nothing on the queue
         self::assertEquals(0, $results->get()->count());
         Queue::assertNothingPushed();
+    }
 
-        // @todo 1 to add to the queue (done is not null)
+    public function testOneJobToCalculate(): void
+    {
+        Queue::fake();
+
+        // add to the queue (done is not null)
         $start = CarbonImmutable::parse('2022-05-23 08:00:00');
         $done = CarbonImmutable::parse('2022-05-27 09:00:00');
         /** @var $issue */
@@ -41,10 +46,27 @@ class CycleTimeCalculateTest extends TestCase
         $results = Issue::needsNewCycletime();
         self::assertEquals(1, $results->get()->count());
         $this->artisan('cycletime:calculate')->assertSuccessful();
-        Queue::assertPushed(UpdateCycleTime::class, 1);
+        Queue::assertPushed(UpdateCycleTime::class);
+    }
 
-        // @todo 1 to not add to the queue (done is null)
+    public function testOneJobOnlyToCalculate(): void
+    {
+        Queue::fake();
 
+        // add to the queue (done is not null)
+        $start = CarbonImmutable::parse('2022-05-23 08:00:00');
+        $done = CarbonImmutable::parse('2022-05-27 09:00:00');
+        /** @var $issue */
+        Issue::factory()->has(Transition::factory(['start' => $start, 'done' => $done]))->create(
+            ['issue_id' => 'fake', 'last_jira_update' => CarbonImmutable::now()]
+        );
+        Issue::factory()->has(Transition::factory())->create(
+            ['issue_id' => 'fake2', 'last_jira_update' => CarbonImmutable::now()]
+        );
+        $results = Issue::needsNewCycletime();
+        self::assertEquals(1, $results->get()->count());
+        $this->artisan('cycletime:calculate')->assertSuccessful();
+        Queue::assertPushed(UpdateCycleTime::class);
     }
 
 
