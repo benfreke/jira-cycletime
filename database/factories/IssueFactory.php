3580100e0d317dbd903370dd41fa367eba580bee
Ben <benfreke@users.noreply.github.com>
2022-09-20 20:08:02 +1000

    test: ensure unique values for issue id

--- database/factories/IssueFactory.php
+++ database/factories/IssueFactory.php
@@ -21,7 +21,7 @@ class IssueFactory extends Factory
     {
         return [
             'summary' => $this->faker->name(),
-            'issue_id' => $this->faker->text(5),
+            'issue_id' => $this->faker->unique()->text(5),
             'last_jira_update' => $this->faker->dateTimeInInterval('-1 month', '+20 days'),
             'assignee' => null,
             'project' => explode(',', config('cycletime.jira-categories'))[0],
